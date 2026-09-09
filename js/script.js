488774c22f864e05034d96bf1afe0369bd0136d9
FRANCO TICONA TICONA <franco.ticona@senaisp.edu.br>
2026-09-09 16:34:17 -0300

    Finalizadoe

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
 let tamanhoGrande = false;
- function aumentarTexto() {
-            document.body.classList.add("texto-grande");
-            tamanhoGrande = true;
- 
+
+function aumentarTexto() {
+    document.body.classList.add("texto-grande");
+    document.body.classList.remove("texto-pequeno");
+    tamanhoGrande = true;
+}
+
+function diminuirTexto() {
+    document.body.classList.remove("texto-grande");
+    document.body.classList.add("texto-pequeno");
+    tamanhoGrande = false;
+}
+
+function altoContraste(){
+    document.body.classList.toggle("contraste")
+
+}
+
+function lerPagina(){
+    speechSynthesis.cancel();
+    const texto = document.getElementById("conteudo").innerText;
+    const leitura =  new SpeechSynthesisUtterance(texto);
+    leitura.lang = "es-ES";
+    leitura.rate = 0.9;
+    speechSynthesis.speak(leitura);
+    
+}
+function pararLeitura() {
+    speechSynthesis.cancel();
+}
 
 
- }
 
- 
